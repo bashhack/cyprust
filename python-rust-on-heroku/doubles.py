21b3481c19077c1328dd2d04094527c2f794c1e1
Marc Laughton (bashhack) <info@marclaughton.com>
2018-04-05 00:19:08 -0400

    Got initial Rust lib imported and working

--- python-rust-on-heroku/doubles.py
+++ python-rust-on-heroku/doubles.py
@@ -1,6 +1,9 @@
+import random
 import re
 import string
-import random
+from itertools import tee
+
+import rustlib
 
 
 # Python ZIP
@@ -20,6 +23,16 @@ def count_doubles_regex(val):
     return len(double_re.findall(val))
 
 
+def count_doubles_functional(val):
+    c1s, c2s = tee(val)
+    next(c2s, None)
+    total = 0
+    for c1, c2 in zip(c1s, c2s):
+        if c1 == c2:
+            total += 1
+    return total
+
+
 # Benchmarking...
 val = ''.join(random.choice(string.ascii_letters) for i in range(1000000))
 
@@ -28,5 +41,17 @@ def test_pure_python(benchmark):
     benchmark(count_doubles, val)
 
 
+def test_functional_python(benchmark):
+    benchmark(count_doubles_functional, val)
+
+
 def test_regex(benchmark):
     benchmark(count_doubles_regex, val)
+
+
+def test_rust(benchmark):
+    benchmark(rustlib.count_doubles, val)
+
+
+def test_rust_fast_af(benchmark):
+    benchmark(rustlib.count_doubles_fast_af, val)
--- python-rust-on-heroku/pyext-rustlib/src/lib.rs
+++ python-rust-on-heroku/pyext-rustlib/src/lib.rs
@@ -8,13 +8,21 @@ use pyo3::prelude::*;
 /// This module is implemend in Rust
 #[py::modinit(rustlib)]
 fn init_mod(py: Python, m: &PyModule) -> PyResult<()> {
-
-    #[pyfn(m, "doubles")]
+    #[pyfn(m, "count_doubles")]
     // pyo3 aware function. All of our python interface could be declared in a separate module.
     // Note that the `#[pyfn()]` annotation automatically converts the arguments from
     // Python objects to rust values; and the Rust return value back into a Python object.
     fn count_doubles_py(val: &str) -> PyResult<u64> {
         let out = count_doubles(val);
+        Ok(out)
+    }
+
+    #[pyfn(m, "count_doubles_fast_af")]
+    // pyo3 aware function. All of our python interface could be declared in a separate module.
+    // Note that the `#[pyfn()]` annotation automatically converts the arguments from
+    // Python objects to rust values; and the Rust return value back into a Python object.
+    fn count_doubles_fast_af_py(val: &str) -> PyResult<u64> {
+        let out = count_doubles_fast_af(val);
         Ok(out)
     }
 
@@ -28,6 +36,22 @@ fn count_doubles(val: &str) -> u64 {
     for (c1, c2) in val.chars().zip(val.chars().skip(1)) {
         if c1 == c2 {
             total += 1;
+        }
+    }
+
+    total
+}
+
+fn count_doubles_fast_af(val: &str) -> u64 {
+    let mut total = 0u64;
+    let mut chars = val.bytes();
+
+    if let Some(mut c1) = chars.next() {
+        for c2 in chars {
+            if c1 == c2 {
+                total += 1;
+            }
+            c1 = c2;
         }
     }
 
