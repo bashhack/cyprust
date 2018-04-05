93857d68273f68853c339b66e3f79291d9d7f1a9
Marc Laughton (bashhack) <info@marclaughton.com>
2018-04-04 22:25:02 -0400

    Initial pyo3 binding

---
+++ python-rust-on-heroku/pyext-rustlib/src/lib.rs
@@ -0,0 +1,35 @@
+#![feature(proc_macro, specialization)]
+
+extern crate pyo3;
+use pyo3::prelude::*;
+
+// add bindings to the generated python module
+// N.B.: names: `librustlib` must be the name of `.so` or `.pyd` file
+/// This module is implemend in Rust
+#[py::modinit(rustlib)]
+fn init_mod(py: Python, m: &PyModule) -> PyResult<()> {
+
+    #[pyfn(m, "doubles")]
+    // pyo3 aware function. All of our python interface could be declared in a separate module.
+    // Note that the `#[pyfn()]` annotation automatically converts the arguments from
+    // Python objects to rust values; and the Rust return value back into a Python object.
+    fn count_doubles_py(val: &str) -> PyResult<u64> {
+        let out = count_doubles(val);
+        Ok(out)
+    }
+
+    Ok(())
+}
+
+// logic implemented as a normal rust function
+fn count_doubles(val: &str) -> u64 {
+    let mut total = 0u64;
+
+    for (c1, c2) in val.chars().zip(val.chars().skip(1)) {
+        if c1 == c2 {
+            total += 1;
+        }
+    }
+
+    total
+}
