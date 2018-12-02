adf18b5472eab914ddf5c2b8a54670f6c3e4d988
bashhack (Marc Laughton) <info@marclaughton.com>
2018-12-02 10:51:41 -0500

    Concurrency with mutex and atomics

--- rust_core_concepts/concurrency_threads_channels_mutex_and_arc/src/main.rs
+++ rust_core_concepts/concurrency_threads_channels_mutex_and_arc/src/main.rs
@@ -1,5 +1,6 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
+use std::time::Duration;
 
 // ----------------------------------------------------------------------------
 //
@@ -64,9 +65,9 @@ fn main() {
 
     let vec_for_iteration = vec![1, 2, 3];
 
-    // the `move` keyword forces the closure to reference
-    // data by value rather than by reference
-    // instead of borrowing from vec_for_iteration
+    // The `move` keyword forces the closure to reference
+    // data by value rather than by reference.
+    // Instead of borrowing from vec_for_iteration
     // we force the closure to take from vec_for_iteration
     // and put it inside of the thread, giving it
     // complete ownership. We also guarantee that the main
@@ -84,6 +85,8 @@ fn main() {
     // and a receiver. The transmitter sits upstream, where we push
     // the message in, and the receiver is where the message comes out.
 
+    // NOTE: `mpsc` =  multiple producer single consumer
+
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
@@ -92,6 +95,131 @@ fn main() {
 
     // NOTE: `recv` is a blocking method, preventing the main thread
     //       from processing any additional computation until the
-    //       channel is resolved
+    //       channel is resolved - `try_recv`
     println!("got {}", rx.recv().unwrap());
+
+    // In the following, we use channels to send data between threads.
+    // We have also written an example of the "multiple producer,
+    // single consumer" pattern. Specifically, we have 24 cloned
+    // transmitters and a single receiver
+    const NUM_TIMERS: usize = 24;
+
+    fn timer(d: usize, tx: mpsc::Sender<usize>) {
+        thread::spawn(move || {
+            println!("{}: setting timer...", d);
+            thread::sleep(Duration::from_secs(d as u64));
+            println!("{}: sent!", d);
+            tx.send(d).unwrap()
+        });
+    }
+
+    let (tx, rx) = mpsc::channel();
+
+    for i in 0..NUM_TIMERS {
+        timer(i, tx.clone());
+    }
+
+    for v in rx.iter().take(NUM_TIMERS) {
+        println!("{}: received!", v);
+    }
+
+    // In addition to channels, Rust also uses the
+    // Mutex (mutual exclusion) abstraction to
+    // communicate shared memory inside of its
+    // concurrency model.
+    //
+    // A Mutex exists to ensure that a piece of data
+    // is only accessible by one thread at a time.
+    //
+    // A Mutex has two primary rules:
+    // (1) the thread that wants access the data must
+    //     acquire the lock of the mutex
+    // (2) once the data is released, you must unlock the data
+    //     so other threads can gain access
+    //
+    // A mutex functions like a storage locker -
+    // one key, multiple persons with access - each person must
+    // have access to the key, however, and if a request for access
+    // is granted, the key must be released from the last person
+    // with access.
+    //
+    // It acts like a smart pointer (i.e., like Box) - specifically,
+    // a mutex lock method returns a smart pointer called a MutexGuard.
+    // This MutexGuardis an RAII implementation of a "scoped lock"
+    // of a mutex, when this data structure is dropped from memory
+    // the lock is removed.
+
+    // NOTE: `Arc` = Atomically Referenced Counted type
+    // (i.e., acts like a primitive, but is safe to share across threads)
+    let c = Arc::new(Mutex::new(0));
+    let mut hs = vec![];
+
+    for _ in 0..10 {
+        let c = Arc::clone(&c);
+        let h = thread::spawn(move || {
+            let mut num = c.lock().unwrap();
+
+            *num += 1;
+        });
+        hs.push(h);
+    }
+
+    for h in hs {
+        h.join().unwrap();
+    }
+
+    println!("Result: {}", *c.lock().unwrap());
+
+    // In summary:
+    // Each thread is spawned, when it is spawned it gains
+    // access to the mutex, increments mutex by one, then releases its
+    // access, then the next thread gains it access and releases, etc.,
+    // After all threads resolve with join, our main thread gets access
+    // to the mutex and prints it out.
+
+    // Example of using channels and mutexes together to find primes:
+
+    fn is_prime(n: usize) -> bool {
+        (2..n).all(|i| n % i != 0)
+    }
+
+    fn producer(tx: mpsc::SyncSender<usize>) -> thread::JoinHandle<()> {
+        thread::spawn(move || {
+            for i in 100_000_000.. {
+                tx.send(i).unwrap();
+            }
+        })
+    }
+
+    fn worker(id: u64, shared_rx: Arc<Mutex<mpsc::Receiver<usize>>>) {
+        thread::spawn(move || loop {
+            {
+                let mut n = 0;
+                match shared_rx.lock() {
+                    Ok(rx) => match rx.try_recv() {
+                        Ok(_n) => {
+                            n = _n;
+                        }
+                        Err(_) => (),
+                    },
+                    Err(_) => (),
+                }
+
+                if n != 0 {
+                    if is_prime(n) {
+                        println!("worker {} found a prime: {}", id, n);
+                    }
+                }
+            }
+        });
+    }
+
+    let (tx, rx) = mpsc::sync_channel(1024); // memory allocation
+    let shared_rx = Arc::new(Mutex::new(rx));
+
+    for i in 1..5 {
+        worker(i, shared_rx.clone());
+    }
+
+    producer(tx).join().unwrap();
 }
